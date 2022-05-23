673742008699380afa76bf6646c925cd62fced5a
bodascaluca <bodasca.luca@gmail.com>
2022-05-23 16:04:21 +0200

    Start to put the Card

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <template>
   <div id="app">
 
+    <input type="text" placeholder="cerca film">
+    <button>Cerca</button>
+
+    <FilmCards />
 
   </div>
 </template>
 
 <script>
-
+import FilmCards from "./components/FilmCards.vue" ;
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-
+  components:{
+    FilmCards,
+  },
+  data(){
+    return {
+      films:[],
+    }
+  },
+  created(){
+    axios.get('https://api.themoviedb.org/3/search/movie?api_key=e1cde3e49c4b61eacdb30a0c144e677b&query=spring break')
+    .then((resp)=>{
+      console.log(resp);
+      this.films=resp.data.results;
+      console.log(this.films);
+    })
   }
 }
 </script>
 
 <style lang="scss">
+@import "./style/common.scss"
+
 
 </style>
---
+++ src/components/FilmCards.vue
@@ -0,0 +1,26 @@
+<template>
+    <div >
+        <div class="card">
+         <h1>Titolo</h1>
+        <h2>Titolo Originale</h2>
+        <h3>Lingue</h3>
+        <h4>Voto</h4>
+        </div>
+    
+    </div>
+</template>
+
+<script>
+export default {
+    name:"FilmCards",
+}
+</script>
+
+<style scoped lang="scss">
+@import "../style/common.scss";
+    .card{
+        margin:1rem;
+        border:1px solid red;
+        width: 200px;
+    }
+</style>
