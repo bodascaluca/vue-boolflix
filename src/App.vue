e9ca4824f8f86b7241a8e615264e092caff114c5
bodascaluca <bodasca.luca@gmail.com>
2022-05-25 15:41:05 +0200

    Poteva essere tutto rosso, ma non lo è(NON FUNZIONA)

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,11 @@
 <template>
   <div id="app">
 
-    <div class="header">
-      <div class="header-left">
-        <img class="img-luflix" src="./assets/img/luflix.png" alt="">
-      </div>
-      <div class="header-right">
-        <input type="text" placeholder="cerca film" v-model="searchWords">
-        <button @click="searchFilms()">Cerca</button>
-      </div>
-    </div>
-
      <div>
-      <HeaderTop />
+      <HeaderTop 
+      @recivedfilms="savefilmsArray($event)"
+      @recivedserietvs="saveserietvsArray($event)"
+      />
     </div>
 
     <div class="color">
@@ -25,84 +18,34 @@
 <script>
 import FilmCards from "./components/FilmCards.vue";
 import HeaderTop from "./components/HeaderTop.vue";
-import axios from 'axios';
 
 export default {
   name: 'App',
+  methods:{
+    savefilmsArray(searchwords){
+      this.films=searchwords;
+    },
+    saveserietvsArray(searchwords){
+      this.serietvs=searchwords;
+    },
+  },
+  data: function(){
+    return {
+      films:[],
+      serietvs:[],
+    }
+  },
   components:{
     FilmCards,
     HeaderTop,
   },
-  data(){
-    return {
-      films:[],
-      searchWords:"",
-      serietvs:[]
-    }
-  },
-  methods:{
-    searchFilms(){
-
-
-       axios
-       .get('https://api.themoviedb.org/3/search/movie', {
-         params:{
-           api_key:"e1cde3e49c4b61eacdb30a0c144e677b",
-           query:this.searchWords,
-         }
-       })
-        //  .get(`https://api.themoviedb.org/3/search/movie?api_key=e1cde3e49c4b61eacdb30a0c144e677b&api_key=e99307154c6dfb0b4750f6603256716d&query=`+ this.searchWords)
-    .then((resp)=>{
-      this.films=resp.data.results;
-    })
-
-
-
-
-        axios
-      .get(`https://api.themoviedb.org/3/search/tv`,{
-        params:{
-          api_key:"e99307154c6dfb0b4750f6603256716d",
-          query:this.searchWords,
-        }
-      })
-   
-    .then((resp)=>{
-      this.serietvs=resp.data.results;
-    })
-    }
-  },
+ 
 }
 </script>
 
 <style lang="scss">
 @import "./style/common.scss";
 
-#app{
-  height:100vh;
-  background: linear-gradient(to bottom,  black 0vh,
-                                          black 15vh,
-                                          grey 15vh,
-                                          grey 100%);
-}
-
-  .header{
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    &-left{
-      margin-left:1rem;
-
-      .img-luflix{
-          width: 200px;
-      }
-    }
-
-    &-right{
-      margin-right:1rem;
-    }
-  }
 
 
 </style>
--- src/components/HeaderTop.vue
+++ src/components/HeaderTop.vue
@@ -1,15 +1,97 @@
 <template>
   <div>
-
+     <div class="header">
+      <div class="header-left">
+        <img class="img-luflix" src="../assets/img/luflix.png" alt="">
+      </div>
+      <div class="header-right">
+        <input type="text" placeholder="cerca film" v-model="searchWords">
+        <button @click="searchFilms()">Cerca</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
+
 export default {
     name:"HeaderTop",
+     data(){
+    return {
+      films:[],
+      searchWords:"",
+      serietvs:[]
+    }
+  },
+  computed:{
+    sendfilmsprova(){
+      return $emit(`recivedfilms`,this.films);
+    },
+    sendserietvsprova(){
+      return $emit(`recivedserietvs`,this.serietvs);
+    }
+  },
+  methods:{
+    searchFilms(){
+       axios
+       .get('https://api.themoviedb.org/3/search/movie', {
+         params:{
+           api_key:"e1cde3e49c4b61eacdb30a0c144e677b",
+           query:this.searchWords,
+         }
+       })
+        //  .get(`https://api.themoviedb.org/3/search/movie?api_key=e1cde3e49c4b61eacdb30a0c144e677b&api_key=e99307154c6dfb0b4750f6603256716d&query=`+ this.searchWords)
+    .then((resp)=>{
+      this.films=resp.data.results;
+    })
+
+
+
+
+        axios
+      .get(`https://api.themoviedb.org/3/search/tv`,{
+        params:{
+          api_key:"e99307154c6dfb0b4750f6603256716d",
+          query:this.searchWords,
+        }
+      })
+   
+    .then((resp)=>{
+      this.serietvs=resp.data.results;
+    })
+    }
+  },
 }
 </script>
 
 <style scoped lang="scss">
+@import "../style/common.scss";
 
+#app{
+  height:100vh;
+  background: linear-gradient(to bottom,  black 0vh,
+                                          black 15vh,
+                                          grey 15vh,
+                                          grey 100%);
+}
+
+  .header{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    &-left{
+      margin-left:1rem;
+
+      .img-luflix{
+          width: 200px;
+      }
+    }
+
+    &-right{
+      margin-right:1rem;
+    }
+  }
 </style>
