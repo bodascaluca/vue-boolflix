a9fef7abe94123020d3f6649d608ea0371deab13
bodascaluca <bodasca.luca@gmail.com>
2022-05-26 16:34:59 +0200

    Add overwie in classs information

--- src/App.vue
+++ src/App.vue
@@ -58,6 +58,7 @@ export default {
   position: fixed;
   top:0;
   left:0;
+  z-index: 999;
 }
 
 .main{
--- src/components/ContentCards.vue
+++ src/components/ContentCards.vue
@@ -2,7 +2,7 @@
   <div class="cover">
 
       <div class="img-container">
-          <img class="img-cover" :src="`https://image.tmdb.org/t/p/w342/${item.poster_path}`" alt="">
+          <img class="img-cover" :src="item.poster_path ? `https://image.tmdb.org/t/p/w342/${item.poster_path}` : require('../assets/img/no-image.jpg') " alt="">
       </div>
      
 
@@ -16,6 +16,7 @@
           <ul class="stars">
               <li v-for="star, index in fammivederelestelle(item.vote_average)" :key="index">&#9733;</li>
           </ul>
+          <p class="paragrafo">{{item.overview}}</p>
       </div>
 
              <!-- <ul class="stars">
@@ -70,16 +71,21 @@ export default {
         border: 1px solid yellow;
         background-color: black;
         height: 100%;
+        overflow: hidden;
+        overflow-y: auto;
+
+       
         .information{
             display:none;
             color:white;
             text-align: center;
-            position: absolute;
-            left:50%;
-            top:50%;
-            transform: translate( - 50% , - 50% );
-            // background-color: black;
-            // padding:1rem;
+            // position: absolute;
+            // left:50%;
+            // top:50%;
+            // transform: translate( - 50% , - 50% );
+            margin: 20px;
+            // margin-bottom: 0px;
+            // vertical-align: top;
 
             .img-flag{
                 width: 20px;
@@ -103,8 +109,13 @@ export default {
             margin: 5px;
             }
         }  
+
+      
     }
 
+//   .paragrafo{
+//             width: 200px;
+//         }
 
         .cover:hover .information{
             display:block;
